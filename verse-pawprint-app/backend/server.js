5d374f416d5415285181d7d6de53f13a2fac5fc2
Mike Backhouse <mikebackhouse@hotmail.co.uk>
2020-02-23 12:31:35 +0000

    database connection setup

--- verse-pawprint-app/backend/server.js
+++ verse-pawprint-app/backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dotenv').config;
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
 app.listen(port, () => {
-    console.log('Server is running on port: ${port}');
+    console.log(`Server is running on port: ${port}`);
 });
